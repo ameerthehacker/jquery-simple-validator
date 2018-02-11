74c625ede16c60492beec4706c23d0e86da49988
ameerthehacker <ameerjhanprof@gmail.com>
2018-02-11 15:25:14 +0530

    Add validations for select :white_check_mark:

--- src/js/validator.js
+++ src/js/validator.js
@@ -23,7 +23,7 @@ export const validateForms = () => {
 const validateForm = form => {
   // Get all the inputs in the form
   const inputs = $(form)
-    .find("input, textarea")
+    .find("input, textarea, select")
     .toArray();
   validateInputs(inputs);
   // Check during form submission and prevent if invalid
