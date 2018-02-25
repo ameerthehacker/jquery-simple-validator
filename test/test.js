594828327f7ed4c190c6000ba0079ca933a59ca2
ameerthehacker <ameerjhanprof@gmail.com>
2018-02-26 00:12:47 +0530

    Add test for required field validation :fire:

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 import { validationFn } from "../src/js/validations";
 import $ from "jquery";
+import { validateForms } from "../src/js/validator";
 
 // Setup jQuery
 beforeAll(() => {
@@ -31,7 +32,7 @@ describe("Testing Validation Functions", () => {
         validationFn.url({ value: "http://www.example.com" })
       ).toBeTruthy();
     });
-    test("Valid URL should return false", () => {
+    test("Invalid URL should return false", () => {
       expect(validationFn.url({ value: "http:/www.example.com" })).toBeFalsy();
     });
   });
@@ -51,6 +52,77 @@ describe("Testing Validation Functions", () => {
     });
     test("Non numeric value should return false", () => {
       expect(validationFn.isNumber({ value: "alpha" })).toBeFalsy();
+    });
+  });
+});
+
+describe("Testing the DOM activities", () => {
+  let $form1 = {},
+    $form2 = {};
+  let $requiredField = {};
+
+  beforeEach(() => {
+    // Set the required html content
+    document.body.innerHTML = `
+      <form id="form1" validate="true">
+        <input id="required-field" type="text" required>
+      </form>
+      <form id="form2">
+      </form>
+    `;
+    validateForms();
+    // Capture the form objects
+    $form1 = $("#form1");
+    $form2 = $("#form2");
+    $requiredField = $("#required-field");
+  });
+  // Test for validating forms
+  describe("Testing form validator", () => {
+    test("Should add novalidate to all forms with validate=true", () => {
+      expect($form1.attr("novalidate")).toBeTruthy();
+    });
+    test("Should not add novalidate to forms without validate=true", () => {
+      expect($form2.attr("novalidate")).toBeFalsy();
+    });
+  });
+  describe("Testing required validator", () => {
+    test("Should add error class when invalid", () => {
+      expect($requiredField.hasClass("error")).toBeFalsy();
+      // Focus and remove focus from the field
+      $requiredField.focus();
+      $requiredField.blur();
+      expect($requiredField.hasClass("error")).toBeTruthy();
+    });
+    test("Should remove error class when valid", () => {
+      $requiredField.addClass("error");
+      $requiredField.focus();
+      $requiredField.val("something");
+      $requiredField.blur();
+      expect($requiredField.hasClass("error")).toBeFalsy();
+    });
+    test("Should add no-error class when valid", () => {
+      $requiredField.focus();
+      $requiredField.val("something");
+      $requiredField.blur();
+      expect($requiredField.hasClass("no-error")).toBeTruthy();
+    });
+    test("Should remove no-error class when invalid", () => {
+      $requiredField.addClass("error");
+      $requiredField.focus();
+      $requiredField.blur();
+      expect($requiredField.hasClass("no-error")).toBeFalsy();
+    });
+    test("Should add error container if invalid", () => {
+      $requiredField.focus();
+      $requiredField.blur();
+      expect($requiredField.next(".error-field").length).toBe(1);
+    });
+    test("Should show error message", () => {
+      $requiredField.focus();
+      $requiredField.blur();
+      expect($requiredField.next(".error-field").text()).toEqual(
+        "This field is required"
+      );
     });
   });
 });
